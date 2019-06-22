6f9c7be25d0c6dfa5e43ee76f778f076d041db79
Logan Stillings <lstillin@u.rochester.edu>
2019-06-22 18:13:09 -0400

    Recursively render subInputs

--- src/app/form-builder/form-builder.js
+++ src/app/form-builder/form-builder.js
@@ -2,20 +2,154 @@ import React from 'react';
 import './form-builder.css';
 
 function SaveButton(props) {
-    return <button onClick={props.onSave}>Save</button>;
+    return (
+        <button className="spaced btn btn-primary" onClick={props.onSave}>
+            Save Form
+        </button>
+    );
 }
 
 function AddInputButton(props) {
-    return <button onClick={props.onInputAdded}>Add Input</button>;
+    return (
+        <button
+            className="spaced btn btn-secondary"
+            onClick={props.onInputAdded}
+        >
+            Add Input
+        </button>
+    );
 }
 
 function FormActionButtons(props) {
     return (
-        <div>
+        <div className="row padded">
             <AddInputButton onInputAdded={props.onInputAdded} />
             <SaveButton onSave={props.onSave} />
         </div>
     );
+}
+
+function Condition(props) {
+    return (
+        <div className="form-group">
+            <div className="inline-block col-md-8">
+                <label htmlFor="ConditionTypeId">Condition</label>
+                <select
+                    name="ConditionTypeId"
+                    className="form-control"
+                    value={props.subInputQuestion.ConditionType}
+                    onChange={() => {}}
+                >
+                    <option>Equals</option>
+                </select>
+            </div>
+            <div className="inline-block col-md-4">
+                <input
+                    className="form-control"
+                    type="text"
+                    value={props.subInputQuestion.ConditionValue}
+                    onChange={() => {}}
+                />
+            </div>
+        </div>
+    );
+}
+
+function SubInputs(props) {
+    return (
+        <div className="sub-inputs-list">
+            {props.subInputs.map((subInputQuestion, index) => {
+                const hasSubInputs = subInputQuestion.SubInputs ? true : false;
+                const key = props.questionId + '_' + props.layer + '_' + index;
+                console.log(key);
+                if (hasSubInputs) {
+                    return (
+                        <div key={key}>
+                            <div className="question-block padded">
+                                <Condition
+                                    subInputQuestion={subInputQuestion}
+                                />
+                                <QuestionBase question={subInputQuestion} />
+                            </div>
+                            <div>
+                                <SubInputs
+                                    layer={props.layer + 1}
+                                    questionId={props.questionId}
+                                    subInputs={subInputQuestion.SubInputs}
+                                />
+                            </div>
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div key={key}>
+                            <div className="question-block padded">
+                                <Condition
+                                    subInputQuestion={subInputQuestion}
+                                />
+                                <QuestionBase question={subInputQuestion} />
+                            </div>
+                        </div>
+                    );
+                }
+            })}
+        </div>
+    );
+}
+
+function QuestionBase(props) {
+    return (
+        <div>
+            <div className="form-group">
+                <label htmlFor="Question">Question</label>
+                <input
+                    className="form-control"
+                    onChange={() => {}}
+                    value={props.question.Question}
+                    name="Question"
+                    type="text"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="QuestionType">Type</label>
+                <select
+                    className="form-control"
+                    onChange={() => {}}
+                    value={props.question.QuestionType}
+                    name="QuestionType"
+                >
+                    <option value="Text">Text</option>
+                    <option value="YesNo">YesNo</option>
+                    <option value="Number">Number</option>
+                </select>
+            </div>
+            <div className="row padded pull-right">
+                <button type="button" className="spaced btn btn-secondary">
+                    Add Sub-Input
+                </button>
+                <button type="button" className="spaced btn btn-danger">
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
+}
+
+function Questions(props) {
+    return props.questions.map((question) => {
+        return (
+            <div key={question.Id}>
+                <div className="question-block padded">
+                    <QuestionBase question={question} />
+                </div>
+                <SubInputs
+                    questionId={question.Id}
+                    subInputs={question.SubInputs}
+                    layer={1}
+                />
+            </div>
+        );
+    });
 }
 
 class FormBuilder extends React.Component {
@@ -37,11 +171,16 @@ class FormBuilder extends React.Component {
     render() {
         return (
             <div>
-                <h1>Form Builder</h1>
-                <FormActionButtons
-                    onInputAdded={() => this.handleInputAdded()}
-                    onSave={() => this.handleSave()}
-                />
+                <form>
+                    <div className="card padded">
+                        <h1>Form Builder</h1>
+                        <Questions questions={this.state.questions} />
+                    </div>
+                    <FormActionButtons
+                        onInputAdded={() => this.handleInputAdded()}
+                        onSave={() => this.handleSave()}
+                    />
+                </form>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FormBuilder from './app/form-builder/form-builder';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<FormBuilder />, document.getElementById('root'));
