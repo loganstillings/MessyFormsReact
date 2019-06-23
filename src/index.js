e2e3419c5957ca2ce505a2b094b3e9b3efea7511
Logan Stillings <lstillin@u.rochester.edu>
2019-06-23 18:39:24 -0400

    fix import of FormBuilder component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
-import FormBuilder from './form-builder/form-builder';
+import FormBuilder from './components/form-builder/form-builder';
 
 ReactDOM.render(<FormBuilder />, document.getElementById('root'));
