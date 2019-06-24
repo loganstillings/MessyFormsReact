8b90313cc135a67c72a9274a94cc5f560c702466
Logan Stillings <lstillings@milestechnologies.com>
2019-06-24 11:32:11 -0400

    Remove casting as any Added an index signature to IQuestionBase This allows for assigning IQuestionBaseProperties via bracket notation

--- src/interfaces/question-base.d.ts
+++ src/interfaces/question-base.d.ts
@@ -1,8 +1,15 @@
 import { ISubInput } from './sub-input';
 import { QuestionTypesEnum } from '../enums/QuestionTypes';
+import { ConditionTypesEnum } from '../enums/ConditionTypes';
 
 export interface IQuestionBase {
     Question: string;
     QuestionType: QuestionTypesEnum;
     SubInputs: ISubInput[];
+    [key: string]:
+        | string
+        | QuestionTypesEnum
+        | ISubInput[]
+        | ConditionTypesEnum
+        | number;
 }
